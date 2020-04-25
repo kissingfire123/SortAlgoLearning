c8ca992ab1cfea5311dadaaf1eced1677dbd98a5
kisingfire123 <1322591040@qq.com>
2020-04-25 16:56:41 +0800

    [Update to V1.0.3]add MergeSort.h
    添加归并排序MergeSort，目前第一个平均复杂度为O(nLogN)的排序算法
    后续会优化MergeSort
    编辑了git忽略文件，删除了不必要的vs工程生成文件.sdf和.suo

---
+++ SortLearning/SortLearning/MergeSort.h
@@ -0,0 +1,81 @@
+#ifndef __MERGE_SORT_H__
+#define __MERGE_SORT_H__
+
+#include "Common.h"
+
+//升序排列，从小到大
+template <typename T>
+void MergeSort(T arr[], int nLen)
+{
+	__mergeSort(arr, 0, nLen - 1);
+}
+
+
+//对[arr[iLeft],arr[iRight]闭区间范围内进行递归排序
+template <typename T>
+void __mergeSort(T arr[], int iLeft, int iRight)
+{
+	if (iLeft >= iRight)
+	{
+		return;
+	}
+	int iMiddle = (iRight - iLeft) / 2 + iLeft;//不用相加求平均，防止溢出int范围
+	__mergeSort(arr, iLeft, iMiddle);
+	__mergeSort(arr, iMiddle + 1, iRight);
+
+	__mergeOperation(arr, iLeft, iMiddle, iRight);
+}
+
+
+
+//对2段有序数组进行合并操作
+template <typename T>
+void __mergeOperation(T arr[], int iLeft, int iMiddle, int iRight)
+{
+	assert(iLeft <= iMiddle);
+	assert(iMiddle <= iRight);
+
+	T *pSortBuffer = new T[iRight - iLeft + 1];//归并排序需要额外开辟一段空间给合并操作使用
+	memcpy_s(pSortBuffer, (iRight - iLeft + 1)*sizeof(T), &arr[iLeft], (iRight - iLeft + 1)*sizeof(T));
+
+	int idxL = iLeft;
+	int idxR = iMiddle+1;
+	int kk = iLeft;
+
+	///首先确保左右下标的合法性
+	for (int kk = iLeft; kk <= iRight; kk++)
+	{
+		if (idxL > iMiddle)
+		{
+			arr[kk] = pSortBuffer[idxR - iLeft];
+			idxR++;
+		}
+		else if (idxR > iRight)
+		{
+			arr[kk] = pSortBuffer[idxL - iLeft];
+			idxL++;
+		}
+		else if (pSortBuffer[idxL - iLeft] < pSortBuffer[idxR - iLeft])
+		{
+			arr[kk] = pSortBuffer[idxL - iLeft];
+			idxL++;
+		}
+		else  /// if (pSortBuffer[idxL - iLeft] >= pSortBuffer[idxR - iLeft])
+		{
+			arr[kk] = pSortBuffer[idxR - iLeft];
+			idxR++;
+		}
+	}
+	SortTestHelper::DeleteRandArr(pSortBuffer);
+}
+
+
+
+
+
+
+
+
+
+
+#endif
--- SortLearning/SortLearning/main.cpp
+++ SortLearning/SortLearning/main.cpp
@@ -3,57 +3,66 @@
 #include "BubbleSort.h"
 #include "SelectionSort.h"
 #include "InsertionSort.h"
+#include "MergeSort.h"
+
+#define SORT_KINDS_NUM 4 
 
 int main()
 {
-	int nArrayLen = 1000;
+	int nArrayLen = 10000;
 	int nSwapTimes = 20;
+	int * ppTestArray[3 * SORT_KINDS_NUM] = { 0 };  //有3种类型的测试数据，并且有 SORT_KINDS_NUM 种待测试的排序方法
 
 	//SortTestHelper::PrintArr("RandomArr", (int*)pRandArr1, nArrayLen, 4);  //For 调试
 	//排序过程
 	//排序一个随机序列
-	int *pRandArr1 = SortTestHelper::NewRandArr<int>(nArrayLen, 0, nArrayLen);
-	int *pRandArr2 = SortTestHelper::NewCopyArray<int>(pRandArr1, nArrayLen);
-	int *pRandArr3 = SortTestHelper::NewCopyArray<int>(pRandArr1, nArrayLen);
+	ppTestArray[0] = SortTestHelper::NewRandArr<int>(nArrayLen, 0, nArrayLen);
+	for (int ii = 1; ii < SORT_KINDS_NUM; ii++)
+	{
+		ppTestArray[ii] = SortTestHelper::NewCopyArray<int>(ppTestArray[0], nArrayLen);
+	}
 	cout << endl << "Sort a array which is complete random :" <<"  DataLen="<<nArrayLen<<endl;
-	SortTestHelper::TestSortPerform("Bubble Sort", BubbleSort   , pRandArr1, nArrayLen);
-	SortTestHelper::TestSortPerform("Select Sort", SelectionSort, pRandArr2, nArrayLen);
-	SortTestHelper::TestSortPerform("Insert Sort", InsertionSort, pRandArr3, nArrayLen);
+	SortTestHelper::TestSortPerform("Bubble Sort", BubbleSort,    ppTestArray[0], nArrayLen);
+	SortTestHelper::TestSortPerform("Select Sort", SelectionSort, ppTestArray[1], nArrayLen);
+	SortTestHelper::TestSortPerform("Insert Sort", InsertionSort, ppTestArray[2], nArrayLen);
+	SortTestHelper::TestSortPerform("Merge  Sort", MergeSort,     ppTestArray[3], nArrayLen);
 
 	//排序一个近乎有序的序列
 	cout << endl << "Sort a array which is nearly sorted :" << "  DataLen=" << nArrayLen << endl;
-	int *pAlmostRandArr1 = SortTestHelper::NewAlmostRandArr<int>(nArrayLen, 0, 1, nSwapTimes);
-	int *pAlmostRandArr2 = SortTestHelper::NewCopyArray<int>(pAlmostRandArr1, nArrayLen);
-	int *pAlmostRandArr3 = SortTestHelper::NewCopyArray<int>(pAlmostRandArr1, nArrayLen);
-	SortTestHelper::TestSortPerform("Bubble Sort", BubbleSort   , pAlmostRandArr1, nArrayLen);
-	SortTestHelper::TestSortPerform("Select Sort", SelectionSort, pAlmostRandArr2, nArrayLen);
-	SortTestHelper::TestSortPerform("Insert Sort", InsertionSort, pAlmostRandArr3, nArrayLen);
-
-
+	ppTestArray[SORT_KINDS_NUM] = SortTestHelper::NewAlmostRandArr<int>(nArrayLen, 0, 1, nSwapTimes);
+	for (int ii = SORT_KINDS_NUM + 1; ii < 2*SORT_KINDS_NUM; ii++)
+	{
+		ppTestArray[ii] = SortTestHelper::NewCopyArray<int>(ppTestArray[SORT_KINDS_NUM], nArrayLen);
+	}
+	SortTestHelper::TestSortPerform("Bubble Sort", BubbleSort,    ppTestArray[SORT_KINDS_NUM],     nArrayLen);
+	SortTestHelper::TestSortPerform("Select Sort", SelectionSort, ppTestArray[SORT_KINDS_NUM + 1], nArrayLen);
+	SortTestHelper::TestSortPerform("Insert Sort", InsertionSort, ppTestArray[SORT_KINDS_NUM + 2], nArrayLen);
+	SortTestHelper::TestSortPerform("Merge  Sort", MergeSort,     ppTestArray[SORT_KINDS_NUM + 3], nArrayLen);
 
 	//排序一个有大量重复元素的序列
 	cout << endl << "Sort a array which contains many same element :" << "  DataLen=" << nArrayLen << endl;
-	int *pRepeatedEleArr1 = SortTestHelper::NewRandArr<int>(nArrayLen, 0, 5);
-	int *pRepeatedEleArr2 = SortTestHelper::NewCopyArray<int>(pRepeatedEleArr1, nArrayLen);
-	int *pRepeatedEleArr3 = SortTestHelper::NewCopyArray<int>(pRepeatedEleArr1, nArrayLen);
-	SortTestHelper::TestSortPerform("Bubble Sort", BubbleSort, pRepeatedEleArr1, nArrayLen);
-	SortTestHelper::TestSortPerform("Select Sort", SelectionSort, pRepeatedEleArr2, nArrayLen);
-	SortTestHelper::TestSortPerform("Insert Sort", InsertionSort, pRepeatedEleArr3, nArrayLen);
+	ppTestArray[2 * SORT_KINDS_NUM] = SortTestHelper::NewRandArr<int>(nArrayLen, 0, 5);
+	for (int ii = 2 * SORT_KINDS_NUM + 1; ii < 3 * SORT_KINDS_NUM; ii++)
+	{
+		ppTestArray[ii] = SortTestHelper::NewCopyArray<int>(ppTestArray[2 * SORT_KINDS_NUM], nArrayLen);
+	}
 
+	SortTestHelper::TestSortPerform("Bubble Sort", BubbleSort,    ppTestArray[2 * SORT_KINDS_NUM],     nArrayLen);
+	SortTestHelper::TestSortPerform("Select Sort", SelectionSort, ppTestArray[2 * SORT_KINDS_NUM + 1], nArrayLen);
+	SortTestHelper::TestSortPerform("Insert Sort", InsertionSort, ppTestArray[2 * SORT_KINDS_NUM + 2], nArrayLen);
+	SortTestHelper::TestSortPerform("Merge  Sort", MergeSort,     ppTestArray[2 * SORT_KINDS_NUM + 3], nArrayLen);
 
-	SortTestHelper::DeleteRandArr <int>(pRepeatedEleArr1);
-	SortTestHelper::DeleteRandArr <int>(pRepeatedEleArr2);
-	SortTestHelper::DeleteRandArr <int>(pRepeatedEleArr3);
-	SortTestHelper::DeleteRandArr <int>(pAlmostRandArr1);
-	SortTestHelper::DeleteRandArr <int>(pAlmostRandArr2);
-	SortTestHelper::DeleteRandArr <int>(pAlmostRandArr3);
-	SortTestHelper::DeleteRandArr <int>(pRandArr1);
-	SortTestHelper::DeleteRandArr <int>(pRandArr2);
-	SortTestHelper::DeleteRandArr <int>(pRandArr3);
+	for (int ii = 0; ii < 3 * SORT_KINDS_NUM; ii++)
+	{
+		SortTestHelper::DeleteRandArr <int>(ppTestArray[ii]);
+	}
+
+	cout << endl << "There are all results above ..." << endl;
 	getchar();
 	return 0;
 }
 
+///         以下所以结果都是我自己的旧电脑，应该是会偏慢，i3-2330M的hp电脑（自己加了ssd和内存），8年前买的了[捂脸]
 //运行结果如下： V1.0.1
 /*
 Sort a array which is complete random : DataLen = 1000
@@ -75,7 +84,7 @@ Insert Sort : 0.04s
 
 
 //运行结果如下： V1.0.2
-//优化了选择排序
+//优化了插入排序
 /*
 Sort a array which is complete random : DataLen = 1000
 Bubble Sort : 0.056s
@@ -92,3 +101,45 @@ Bubble Sort : 0.033s
 Select Sort : 0.002s
 Insert Sort : 0.001s
 */
+
+
+//运行结果如下： V1.0.3
+//加入了归并排序(未优化的归并)
+/*数据长度1000
+Sort a array which is complete random : DataLen = 1000
+Bubble Sort : 0.056s
+Select Sort : 0.002s
+Insert Sort : 0.001s
+Merge  Sort : 0.002s
+
+Sort a array which is nearly sorted : DataLen = 1000
+Bubble Sort : 0.004s
+Select Sort : 0.002s
+Insert Sort : 0s
+Merge  Sort : 0.002s
+
+Sort a array which contains many same element : DataLen = 1000
+Bubble Sort : 0.035s
+Select Sort : 0.002s
+Insert Sort : 0.001s
+Merge  Sort : 0.003s
+*/
+/* 数据长度10000
+Sort a array which is complete random :  DataLen=10000
+Bubble Sort:4.498s
+Select Sort:0.206s
+Insert Sort:0.106s
+Merge  Sort:0.02s
+
+Sort a array which is nearly sorted :  DataLen=10000
+Bubble Sort:0.183s
+Select Sort:0.197s
+Insert Sort:0s
+Merge  Sort:0.019s
+
+Sort a array which contains many same element :  DataLen=10000
+Bubble Sort:3.72s
+Select Sort:0.195s
+Insert Sort:0.099s
+Merge  Sort:0.02s
+*/  
